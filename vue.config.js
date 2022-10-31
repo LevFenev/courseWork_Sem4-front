a433f6976ba9d55948ded514cd82ced9ab8bee7f
LevFenev <75114523+LevFenev@users.noreply.github.com>
2022-10-31 14:17:03 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "/courseWork_Sem4-front/"
+  transpileDependencies: true
 })
